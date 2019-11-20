174456808dd45347f9ff8fb8b8f3c91102376bcf
Vadim Sadriev <vadimsadrievtw@gmail.com>
2019-11-21 00:25:17 +0500

    Add jwt auth attribute

---
+++ src/backend/Auth.Infrastructure/Auth/Jwt/AuthorizeJwtAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Auth.Infrastructure.Auth.Jwt
+{
+    /// <summary> The authorization policy for token-based authentication </summary>
+    public class AuthorizeJwtAttribute : AuthorizeAttribute
+    {
+        /// <summary> The authorization policy for token-based authentication </summary>
+        public AuthorizeJwtAttribute()
+        {
+            AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+        }
+    }
+}
