9b566853073b3f996dc370ae0bbbd4683ef08bd0
Vadim Sadriev <vadimsadrievtw@gmail.com>
2019-11-24 01:18:49 +0500

    Add unit tests, add auto mapper configuration test

---
+++ src/backend/Auth.UnitTests/AutoMapperTests.cs
@@ -0,0 +1,31 @@
+﻿using Auth.Application;
+using Auth.Infrastructure;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Auth.UnitTests
+{
+    /// <summary> Unit tests for auto mapper </summary>
+    public class AutoMapperTests
+    {
+        /// <summary>
+        /// Test for valid profile configurations
+        /// </summary>
+        [Fact]
+        public void TestConfiguration()
+        {
+            var assemblies = new[]
+            {
+                typeof(InfrastructureExtensions).Assembly,
+                typeof(ApplicationExtensions).Assembly
+            };
+
+            var mapperConfiguration = new MapperConfiguration(x => x.AddMaps(assemblies));
+
+            mapperConfiguration.AssertConfigurationIsValid();
+        }
+    }
+}
