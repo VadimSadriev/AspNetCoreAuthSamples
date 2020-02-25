00211858a3e6a18e11dbc1c4230b351ed609034b
Вадим <SadrievVF@it2g.ru>
2020-02-25 18:33:36 +0500

    use mediatr in controllers

---
+++ src/backend/Auth.Application/Identity/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using Auth.Contracts.AccountContracts;
+using MediatR;
+
+namespace Auth.Application.Identity.Queries
+{
+    /// <summary> Query to return user by id </summary>
+    public class GetUserByIdQuery : IRequest<UserResponseContract>
+    {
+        /// <summary> Query to return user by id </summary>
+        public GetUserByIdQuery(string userId)
+        {
+            UserId = userId;
+        }
+
+        public string UserId { get; set; }
+    }
+}
