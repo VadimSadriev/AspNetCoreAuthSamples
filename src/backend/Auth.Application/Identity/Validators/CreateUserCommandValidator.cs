ca6266e276a9e15c22d65101f9025860ca8784a8
Вадим <SadrievVF@it2g.ru>
2020-02-26 14:17:09 +0500

    add validator for create user command

--- src/backend/Auth.Application/Identity/Validators/CreateUserCommandValidator.cs
+++ src/backend/Auth.Application/Identity/Validators/CreateUserCommandValidator.cs
@@ -1,6 +1,7 @@
 ﻿using Auth.Application.Common.Interfaces.Identity;
 using Auth.Application.Identity.Commands;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Auth.Application.Identity.Validators
 {
@@ -10,7 +11,13 @@ namespace Auth.Application.Identity.Validators
         /// <summary> Validates incoming <see cref="CreateUserCommand"/> </summary>
         public CreateUserCommandValidator(IUserDataContext context)
         {
+            RuleFor(x => x.UserCreateContract.UserName)
+                .MustAsync(async (prop, val, token) => await context.Users.AnyAsync(x => x.UserName == val))
+                .WithMessage(x => $"User with Username: {x.UserCreateContract.UserName} already exists");
 
+            RuleFor(x => x.UserCreateContract.Email)
+                .MustAsync(async (prop, val, token) => await context.Users.AnyAsync(x => x.Email == val))
+                .WithMessage(x => $"User with Email: {x.UserCreateContract.Email} already exists");
         }
     }
 }
